54809290cd1886f6df21bffe904252c40b9329b4
TaN Taa <atan60896@gmail.com>
2023-09-26 12:15:06 +0500

    header end

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,102 @@
+import { useState } from "react";
 import "./style.scss";
+import Dark from "../../assets/images/svg/light.svg";
+import Menu from "../../assets/images/svg/menu.svg";
+import Close from "../../assets/images/svg/close.svg";
 
 const index = () => {
+  const [modal, setModal] = useState(true);
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__logo">
+            <span>{"< AA />"}</span>
+          </div>
+
+          <div className="header__list">
+            <div className="header__list--left">
+              <ul>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Work</a>
+                </li>
+                <li>
+                  <a href="#">Testimonials</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="header__list--right">
+              <button className="header__list--right-img">
+                <img src={Dark} alt="" />
+              </button>
+              <a
+                href="https://drive.google.com/uc?export=download&id=1s7BU_2qZqLHlkqomT1BMvFhuwKvrvQJB"
+                download
+                className="header__list--right-download"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <button
+            className="header__modal--open"
+            onClick={() => setModal(!modal)}
+          >
+            <img src={Menu} alt="Menu" />
+          </button>
+
+          <div
+            className="header__modal"
+            style={
+              modal
+                ? { transform: "translateX(120%)" }
+                : { transform: "translateX(0%)" }
+            }
+          >
+            <div className="header__modal--top">
+              <h3>{"<AA />"}</h3>
+              <img src={Close} alt="Close" onClick={() => setModal(!modal)}/>
+            </div>
+            <div className="header__modal--center">
+              <ul>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Work</a>
+                </li>
+                <li>
+                  <a href="#">Testimonials</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="header__modal--bottom">
+              <div className="header__modal--bottom-theme">
+                <p>Switch Theme</p>
+                <img src={Dark} alt="Dark mode" />
+              </div>
+
+              <a
+                href="https://drive.google.com/uc?export=download&id=1s7BU_2qZqLHlkqomT1BMvFhuwKvrvQJB"
+                download
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
--- src/components/Layout/Intro/Intro.jsx
+++ src/components/Layout/Intro/Intro.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import TypewriterComponent from "typewriter-effect";
+import "./style.scss";
+import location from "../../../assets/images/svg/location.svg";
+import git from "../../../assets/images/svg/github.svg";
+import twit from "../../../assets/images/svg/twitter.svg";
+import figma from "../../../assets/images/svg/figma.svg";
+import background from "../../../assets/images/svg/background.png";
 
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <section id="intro" className="intro">
+      <div className="container">
+        <div className="intro__wrapper">
+          <div className="intro__left">
+            <div className="intro__left--title">
+              <h1>Hi, I’m </h1>
+              <h1>
+                <TypewriterComponent
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                    delay: 100,
+                    strings: [
+                      " Abdulqosim 👋",
+                      " a Web developer",
+                      " a Python developer",
+                    ],
+                  }}
+                />
+              </h1>
+            </div>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+              vitae mollitia ullam eligendi voluptatum laborum quos, vel
+              reiciendis explicabo, quo amet fuga ex corrupti rem incidunt
+              similique et. Modi rem fuga tempore similique ipsam nostrum
+              corporis impedit incidunt ab delectus. Ab ipsa debitis inventore
+              eius? Vero, doloremque voluptates.
+            </p>
+
+            <div className="intro__location">
+              <div>
+                <img src={location} alt="Location" />
+                <p>Tashkent, Chilonozor</p>
+              </div>
+
+              <div>
+                <p className="dot"></p>
+                <p>Available for new projects</p>
+              </div>
+            </div>
+
+            <div className="intro__links">
+              <a href="#">
+                <img src={git} alt="Github" />
+              </a>
+              <a href="#">
+                <img src={twit} alt="Twitter" />
+              </a>
+              <a href="#">
+                <img src={figma} alt="Figma" />
+              </a>
+            </div>
+          </div>
+          <div className="intro__right">
+            <img className="one" src="https://picsum.photos/280/320" alt="" />
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
